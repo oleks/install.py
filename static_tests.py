ba5b0c4b4f81af18fcc31861a8aeaa144deb627c
oleks <oleks@oleks.info>
2017-06-03 22:25:25 +0200

    Test the hooks files one at a time, as mypy is silly

--- static_tests.py
+++ static_tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os.path
 import subprocess
 import sys
 from typing import List
@@ -15,11 +16,22 @@ def run(command: List[str]) -> None:
         exitcode = 1
 
 
-run(["flake8", "."])
-run([
-    "mypy",
-    "--strict",
-    "--ignore-missing-imports",
-    "."])
+extra_files = [
+        os.path.join('hooks', 'pre-commit'),
+        os.path.join('hooks', 'pre-push')
+    ]
+
+run(["flake8", "."] + extra_files)
+
+mypy_cmd = [
+        "mypy",
+        "--strict",
+        "--ignore-missing-imports"
+    ]
+
+run(mypy_cmd + ['.'])
+
+for extra_file in extra_files:
+    run(mypy_cmd + [extra_file])
 
 sys.exit(exitcode)
